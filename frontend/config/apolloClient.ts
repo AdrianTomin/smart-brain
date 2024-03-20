42e6c9e175b1fc3f864ef1cdab1a0bccf90807b6
Adrian <Adrian.tomin@gmail.com>
2024-03-20 03:56:01 -0600

    apolloClient changes

--- frontend/config/apolloClient.ts
+++ frontend/config/apolloClient.ts
@@ -10,7 +10,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
  * @description HTTP link to the GraphQL server.
  */
 const httpLink = new HttpLink({
-	uri: 'http://localhost:4000/graphql',
+	uri: 'https://smart-brain-api-nine.vercel.app/graphql', // for localhost -> http://localhost:4000/graphql
 	credentials: 'include',
 });
 
